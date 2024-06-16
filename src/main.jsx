d71c4a144d5e93c25771906915300fcca57a4dbd
Idba1 <moniraislamself@gmail.com>
2024-06-16 23:13:11 +0600

    setup tailwind and daisy ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Home from './Pages/Home/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <h1 className='text-2xl text-red-600'>Hello! This is Idba....</h1>
+    <Home></Home>
   </React.StrictMode>,
 )
